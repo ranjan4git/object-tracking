eae4333dbad8c7a8bb405f6e79378bb04176247a
Abhishek <ranjan29abhi@gmail.com>
2023-07-03 18:46:30 +0530

    Parsing arguments

--- Ball_tracking.py
+++ Ball_tracking.py
@@ -1,8 +1,11 @@
-from collections import deque
-from imutils.video import VideoStream
 import numpy as np
 import argparse
 import cv2
-import imutils
-import time
 
+#Construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video",
+                help="path to the (optimal) video file")
+ap.add_argument("-b", "--buffer", type=int, default=64,
+                help="max buffer size")
+args = vars(ap.parse_args())
