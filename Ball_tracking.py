e5e180020cda11451748baa7c57f4c576cb84b09
Abhishek <ranjan29abhi@gmail.com>
2023-07-03 18:58:41 +0530

    Define and track HSV boundary

--- Ball_tracking.py
+++ Ball_tracking.py
@@ -1,3 +1,4 @@
+from collections import deque
 import numpy as np
 import argparse
 import cv2
@@ -9,3 +10,10 @@ ap.add_argument("-v", "--video",
 ap.add_argument("-b", "--buffer", type=int, default=64,
                 help="max buffer size")
 args = vars(ap.parse_args())
+
+# define the lower and upper boundaries in the HSV color space
+greenLower = (29, 86, 6)
+greenUpper = (64, 255, 255)
+
+# initialize the list of tracked points
+pts = deque(maxlen=args["buffer"])
