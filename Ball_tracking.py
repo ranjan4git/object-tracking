ddd176672aff9f4fd092442c79f1dd89d0ec357e
Abhishek <ranjan29abhi@gmail.com>
2023-07-03 21:39:19 +0530

    contour detection and centroid tracking

--- Ball_tracking.py
+++ Ball_tracking.py
@@ -54,3 +54,28 @@ while True:
     mask = cv2.inRange(hsv, greenLower, greenUpper)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
+
+    # find contour (outline) in mask and initialize centre (X,y) of ball
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    center = None
+
+    # if a contour is found
+    if len(cnts) > 0:
+        # find largest contour, compute minimum enclosing circle and centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x,y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+        #if radius exceeds a minimum value
+        if radius > 10:
+            # draw the circle and centroid on the frame
+            cv2.circle(frame, (int(x), int(y)), int(radius),
+                       (0,255,255), 2)
+            cv2.circle(frame, center, 5, (0,0,255), -1)
+
+    # update the list of tracked points
+    pts.appendleft(center)
+    
